e28c6cadde1fb19d2e5e08ba828aec83d5ddbb63
isabelle2m3 <00001129761022sp@al.educacao.sp.gov.br>
2024-11-28 11:54:40 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -15,3 +15,22 @@ criaCartao(
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
+
+criaCartao(
+    'Matemática',
+    'Quantas casas decimais tem o numero pi?
+    'infinitas'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Australia?',
+    'A capital da Australia é Camberra'
+)
+
+criaCartao(
+    'Historia',
+    'Em que período da pré-história o fogo foi descoberto?',
+    'Foi no Paleolítico que o fogo começou a ser utilizado'
+)
+
